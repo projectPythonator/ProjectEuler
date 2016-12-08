9ac082792279f38311f90e497a9ca4d795d7cf34
projectPythonator <ard902@mail.usask.ca>
2016-12-07 23:02:22 -0600

    p10

--- p10.cc
+++ p10.cc
@@ -1,12 +1,16 @@
 //author agis daniels
 //goal is Find the sum of all the primes below two million.?
-#include <iostream> // cin cout 
-#include <math.h> // sqrt 
+#include <iostream> // cin cout
+#include <math.h> // sqrt
 #include <bitset> // for bit set
 #include <vector> // for the vector
 
 using namespace std;
 
+//is_prime(int n)
+//this is a function that can be used uses trivial division
+//param n is the number to check
+//return true if prime false otherwise
 bool is_prime(int n){
     if(!(n%3)){
         return false;
@@ -20,18 +24,31 @@ bool is_prime(int n){
     return true;
 }
 
-vector<int> get_seive(const int size){
+//get_sieve of eratosthenes(const int size)
+//sieve of eratosthenes the bool part of it
+//param size is the size we would like to cover
+//return the seive in a bit set
+bitset<1000000> get_sieve_of_eratosthenes(const int size){
+    bitset<1000000> seive;
     const int size2 = size/2;
-    bitset<(1000000)> seive;
-    int r = (sqrt(size)-1)/2;
+    const int r     = (sqrt(size)-1)/2;
     seive.set(0);
     for(int i=1; i<=r; ++i){
         if(0==seive[i]){
-            for(int j=i*(i+1)+i*(i+1); j<size2; j+=i+i+1){
+            for(int j=i*(i+1)+i*(i+1); j<size2; j+=(2*i+1)){
                 seive.set(j);
             }
         }
     }
+    return seive;
+}
+
+//get_sieve(const int size)
+//sieve of eratosthenes the vector part
+//param size used for the size of the sieve
+//return the seive in a vector
+vector<int> get_seive(const int size){
+    bitset<1000000> seive = get_sieve_of_eratosthenes(size);
     vector<int> rVec;
     rVec.push_back(2);
     for(int i=999999; i>0; --i){
@@ -41,9 +58,10 @@ vector<int> get_seive(const int size){
     }
     return rVec;
 }
+
 int main(){
     vector<int> vec_seive = get_seive(2000000);
-    long sum_primes = 0;
+    long sum_primes    = 0;
     int prime_set_size = vec_seive.size();
     for(int i = prime_set_size-1 ; i >= 0 ; --i){
         sum_primes += vec_seive[i];
