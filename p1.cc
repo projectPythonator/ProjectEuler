21451f461f9c3f9fba37de19b8db5d0ddd63a6aa
projectPythonator <ard902@mail.usask.ca>
2017-10-16 19:39:33 -0600

    Update p1.cc

--- p1.cc
+++ p1.cc
@@ -1,19 +1,32 @@
 //author agis daniels
 //goal is Find the sum of all the multiples of 3 or 5 below 1000.
-#include <iostream> // cin cout 
+#include <stdio.h> 
 
-using namespace std;
-
-int main(){
-	int up_lim=0, tot_sum=0 ;
-	cout << "enter the upper limit"<<endl;
-	cin >> up_lim;
-	//loop for 0 to up_lim checking if i is div by 3 or 5 and summing those up
-	for(int i=0; i<up_lim; ++i){
-		if((i%3==0)||(i%5==0)){
+void sol1(int up_lim){
+	int tot_sum = 0;
+	for(int i = 0; i < up_lim; ++i){
+		if( (i%3 == 0) || (i%5 == 0) ){
 			tot_sum+=i;
 		}
 	}
-	cout << "sum of numbers below " << up_lim <<" is " << tot_sum << endl;
+	printf("The sum of numbers below %d is %d.\n", up_lim, tot_sum); 
+}
+
+int sol2Helper(int n, int up_lim){
+	return 	n*((up_lim-1)/n)*(((up_lim-1)/n)+1)/2;
+}
+
+void sol2(int up_lim){
+	int threes, fives, fiffteens;
+	threes    = sol2Helper(3, up_lim);
+	fives     = sol2Helper(5, up_lim);
+	fiffteens = sol2Helper(15, up_lim);
+	printf("The sum of numbers below %d is %d.\n", up_lim, threes + fives - fiffteens); 
+}
+
+int main(){
+	int up_lim = 1000;
+	sol1(up_lim);
+	sol2(up_lim);
 	return 0;
 }
